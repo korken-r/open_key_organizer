0ad6fd2da51113d26a14ab2b7dec81f83966264c
Peter <git@korken-r.de>
2025-10-05 23:02:56 +0200

    linear option

--- include/globals.h
+++ include/globals.h
@@ -5,6 +5,14 @@
 #define MAX_KEYS 22
 #define LED_PER_KEY 2
 
+#define LED_POS 1,2,5,6,9,10,13,14,17,18,21,0,3,4,7,8,11,12,15,16,19,20
+#define KEY_POS 16,17,18,19,20,12,13,14,15,0,1,21,8,9,10,11,2,3,4,5,6,7
+
+//#define LED_POS 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21
+//#define KEY_POS 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21
+
+
+
 // adapt used pins here, depending on esp flavor
 #if defined(ARDUINO_ESP8266_ADAFRUIT_HUZZAH)
 #define DATA_PIN     14
@@ -20,8 +28,6 @@
 #define NEO_PIN      D5
 #endif
 
-#define LED_POS 1,2,5,6,9,10,13,14,17,18,21,0,3,4,7,8,11,12,15,16,19,20
-#define KEY_POS 16,17,18,19,20,12,13,14,15,0,1,21,8,9,10,11,2,3,4,5,6,7
 
 //mircoseconds
 #define CLOCK_HIGH_DELAY 2
