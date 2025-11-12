a5869f0c2dd7416b5cd2a66ac0a9dd336646d8c3
Peter <git@korken-r.de>
2025-11-13 00:01:10 +0100

    read label names from json file

--- tools/make_labels.py
+++ tools/make_labels.py
@@ -1,30 +1,9 @@
 import drawsvg as draw
 from datetime import datetime
 import argparse
+import json
 
 WRITE_COLOR = '#000000'
-
-schilder = {
-1 : ["Blauer Bund",""],
-2 : ["Roter Bund",""],
-3 : ["Rolltore",""],
-4 :	["Fahrradplatz","TV"],
-5 :	["Ersatzteile","Container"],
-6 :	["CNC Fräse",""],
-7 :	["Lötplätze",""],
-8 :	["Schubladen","Schrank"],
-9 :	["Bohr-","maschine"],
-10 : ["Hänge-","schränke"],
-11 : ["Hänge-","schränke (F)"],
-12 : ["Drechsel-","bank"],
-13 : ["Bandsäge",""],
-14 : ["Handtuch-","spender"],
-15 : ["Kappsäge",""],
-16 : ["Gefahrstoffe",""],
-17 : ["Schrauben-","schrank"],
-18 : ["Keller","3D Druck"],
-19 : ["TrudeTrailer",""]
-}
 
 D = 4.4
 L = 80.0
@@ -33,7 +12,6 @@ A = 2.8
 LINE = 1.0
 OFFSET_TEXT_X = 6.5
 OFFSET_TEXT_Y = 1
-
 
 def calc_offset(i,all_off = (10,10)):
     global L,B
@@ -47,7 +25,7 @@ def calc_offset(i,all_off = (10,10)):
     return (off_x,off_y)
     
 
-def make_one(dr,line1,line2,number,offset,font_size=8):
+def make_one_label(dr,number,line1,line2,offset,font_size=8):
     global D,L,B,LINE,OFFSET_TEXT_X,OFFSET_TEXT_Y
     (x0,y0) = offset
     if line2 != "":
@@ -97,8 +75,13 @@ def make_one(dr,line1,line2,number,offset,font_size=8):
                          stroke_width=LINE))
 d = draw.Drawing(400, 400, origin=(0, 0))
 
-for i in range(19):
+with open('labels.json', 'r') as fp:
+    labels = json.load(fp)
+
+for i,l in enumerate(labels):
+    nmb = l['position']
+   
     offset = calc_offset(i)
-    make_one(d,schilder[i+1][0],schilder[i+1][1],i+1,offset)
+    make_one_label(d,l['position'],l['line1'],l['line2'],offset)
 d.set_pixel_scale(3.7795)
-d.save_svg("schilder.svg")
+d.save_svg("oko_labels.svg")
